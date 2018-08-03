d5e58688d0b05b037db73e927994bc0ff506f5c8
Abhimanyu Dwivedi <abby.dwd@gmail.com>
2018-08-03 19:50:39 +0530

    single entry

--- news_feed.py
+++ news_feed.py
@@ -3,5 +3,15 @@ import requests
 import feedparser
 
 d = feedparser.parse('https://www.hindustantimes.com/rss/topnews/rssfeed.xml')
-print(d['feed']['title'])
-print()
+# print(d['feed']['subtitle'])
+try:
+	for sup in range(len(d['entries'])):
+		print("Title-->"+d['entries'][sup]['title'])
+		print()
+		print("Full story-->"+d['entries'][sup]['summary_detail']['value'])
+		print()
+		print("link-->"+d['entries'][sup]['link'])
+		print()
+
+except Exception as e:
+	raise e
