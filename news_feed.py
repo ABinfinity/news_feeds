382fa3668318e5de878eb28e889b74c0d05f29d4
Abhimanyu Dwivedi <abby.dwd@gmail.com>
2018-08-03 21:57:57 +0530

    entry

--- news_feed.py
+++ news_feed.py
@@ -3,15 +3,24 @@ import requests
 import feedparser
 
 d = feedparser.parse('https://www.hindustantimes.com/rss/topnews/rssfeed.xml')
-# print(d['feed']['subtitle'])
-try:
-	for sup in range(len(d['entries'])):
-		print("Title-->"+d['entries'][sup]['title'])
-		print()
-		print("Full story-->"+d['entries'][sup]['summary_detail']['value'])
-		print()
-		print("link-->"+d['entries'][sup]['link'])
-		print()
+print(d['entries'][1]['title'])
+# print()
+# print(d['entries'][1]['title_detail']['value'])
+print()
+print(d['entries'][1]['summary'])
+# print()
+# print(d['entries'][1]['summary_detail']['value'])
+print()
+print(d['entries'][1]['media_content'][0]['url'])
+print()
+# try:
+# 	for sup in range(len(d['entries'])):
+# 		print("Title-->"+d['entries'][sup]['title'])
+# 		print()
+# 		print("Full story-->"+d['entries'][sup]['summary_detail']['value'])
+# 		print()
+# 		print("link-->"+d['entries'][sup]['link'])
+# 		print()
 
-except Exception as e:
-	raise e
+# except Exception as e:
+# 	raise e
