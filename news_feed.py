cc16b78e5647efba9aaef0fd59624de0e930b4af
Abhimanyu Dwivedi <abby.dwd@gmail.com>
2018-08-07 15:54:02 +0530

    inprocess

--- news_feed.py
+++ news_feed.py
@@ -7,29 +7,38 @@ url_list = {'hindustan_times':'https://www.hindustantimes.com/rss/topnews/rssfee
 			'ndtv_news':'http://feeds.feedburner.com/ndtvnews-top-stories',\
 			'india_times':'https://timesofindia.indiatimes.com/rssfeedstopstories.cms',\
 			'the_hindu':'https://www.thehindu.com/news/feeder/default.rss',\
-			'india_today':'https://www.indiatoday.in/rss/1206584'}
+			'india_today':'https://www.indiatoday.in/rss/1206584',\
+			'reuters':'http://feeds.reuters.com/reuters/INtopNews',\
+			'indian_express':'http://www.newindianexpress.com/Nation/rssfeed/?id=170&getXmlFeed=true',\
+			'livemint':'https://www.livemint.com/rss/homepage',\
+			'b_quint':'https://www.bloombergquint.com/stories.rss',\
+			'ib_times':'https://www.ibtimes.co.in/rss/feed',\
+			'b_today':'https://www.businesstoday.in/rss/rssstory.jsp?sid=105'}
 
 
 
 for key,value in url_list.items():
 	d = feedparser.parse(value)
-	# print("key----->"+key)
-	# print(len(d['entries']))
+	print("key----->"+key)
+	
 	try:
-		for sup in range(len(d['entries'])):
-			title = "Title-->"+d['entries'][sup]['title']
-			
-			short = "Short-->"+d['entries'][sup]['summary_detail']['value']
-			
-			link = d['entries'][sup]['link']
+		# for sup in range(len(d['entries'])):
+		title = "Title-->"+d['entries'][0]['title']
+		
+		short = "Short-->"+d.entries[0].summary
+		
+		link = d['entries'][0]['link']
 
-			paragraph = news_scraper.scraper.scraper(key,link)
-			print(title)
-			print(short)
-			print("link-->"+link)
-			print(paragraph)
+		print(title)
+		print(short)
+		print("link-->"+link)
+		paragraph = news_scraper.scraper.scraper(key,link)
+		print("para-->"+paragraph)
 
 	except Exception as e:
 		raise e
+	print()
+	print()
+	print()
 	
 	
--- news_scraper.py
+++ news_scraper.py
@@ -1,12 +1,43 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 class scraper():
 	def scraper(key,address):
 		url = requests.get(address).text
 		link = BeautifulSoup(url,'lxml')
 		if key == 'hindustan_times':
-			article = link.find('div', class_='story-details').text
+			article = link.find('div', class_='story-details').get_text()
+			return article
+		elif key == 'ndtv_news':
+			article = link.find('div', class_='ins_storybody').get_text()
+			return article
+		elif key == 'india_times':
+			article = link.find('div', class_='Normal').get_text()
+			return article
+		elif key == 'the_hindu':
+			article = link.find('div', id = re.compile('^content-body-')).get_text()
+			return article
+		elif key == 'india_today':
+			article = link.find('div', class_='description').get_text()
+			return article
+		elif key == 'reuters':
+			article = link.find('div', class_='StandardArticleBody_body').get_text()
+			return article
+		elif key == 'indian_express':
+			article = link.find('div', class_='hasrelatedarticle1').get_text()
+			return article
+		elif key == 'livemint':
+			article = link.find('div', class_='content').get_text()
+			return article
+		elif key == 'b_quint':
+			article = link.find('div', class_='story-element story-element-text').get_text()
+			return article
+		elif key == 'ib_times':
+			article = link.find('div', id ='article_content').get_text()
+			return article
+		elif key == 'b_today':
+			article = link.find('div', class_='story-right relatedstory').get_text()
 			return article
 		else:
 			return "null"
